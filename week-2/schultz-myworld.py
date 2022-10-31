aa651340c68d016b3f1bde6c580d7ace2be18f87
Thomas Schultz <tjschultz1@mail.mccneb.edu>
2022-10-30 19:18:29 -0500

    schultz-python
    schultz python push

---
+++ week-2/schultz-myworld.py
@@ -0,0 +1,11 @@
+# WEB 335 Introduction to NoSQL
+# Contributors
+
+# Richard Krasso
+# Thomas James Schultz
+
+#variable with my name
+name = "Thomas Schultz"
+
+#running the program to print the sentence with the variable.
+print("You are in " + name + "'s world now")
