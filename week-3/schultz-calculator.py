4007e436cc99b76799bef095962a9f42d80c730f
Thomas Schultz <tjschultz1@mail.mccneb.edu>
2022-11-02 17:35:05 -0500

    schultz calculator
    here is the calculator

---
+++ week-3/schultz-calculator.py
@@ -0,0 +1,37 @@
+# WEB 335 Introduction to NoSQL
+# Contributors
+
+# Richard Krasso
+# Thomas James Schultz
+
+#This is the add function. Simply taking two integers and adding them.
+#Calling the add function at the end will add the two parameters
+def add(num1, num2):
+  num3 = num1 + num2
+  print(num1, "+" ,num2, "is" ,num3)
+add(4,4)
+
+#This is the subtract function. Simply taking two integers and subtracting them.
+#Calling the subtracting function at the end will subtract the two parameters
+def subtract(num1, num2):
+  num3 = num1 - num2
+  print(num1, "-" ,num2, "is" ,num3)
+subtract(10,6)
+
+#This is the divide function. Simply taking two integers and dividing them.
+#Calling the divide function at the end will divide the two parameters
+def divide(num1, num2):
+  num3 = num1 / num2
+  print(num1, "/" ,num2, "is" ,num3)
+divide(8,2)
+
+#This is the multiply function. Simply taking two integers and multiplying them.
+#Calling the multiply function at the end will multiply the two parameters
+def multiply(num1, num2):
+  num3 = num1 * num2
+  print(num1, "*" ,num2, "is" ,num3)
+multiply(10,2)
+
+#Running this code will show up in the terminal. I will provide a screenshot of my results when I turn this in. 
+
+  
